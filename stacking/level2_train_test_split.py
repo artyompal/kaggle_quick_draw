92f481241dfa6fa537b726aa4bc2fd67ad92e77c
artyom <cppg@cppg.co.uk>
2018-12-01 19:21:53 +0000

    implemented train/test split for level2

---
+++ stacking/level2_train_test_split.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3.6
+""" Converts the data into a format, suitable for training. """
+
+import multiprocessing, os, sys
+from functools import partial
+from glob import glob
+from typing import *
+
+import numpy as np, pandas as pd
+from tqdm import tqdm
+
+TRAIN_IMGS_PER_CLASS = 400
+VAL_IMGS_PER_CLASS = 100
+
+if __name__ == "__main__":
+    big_val = pd.read_csv("val_pavel.csv")
+    small_val = pd.read_csv("val_common_simple.csv")
+
+    train_level2 = []
+    val_level2 = []
+
+    difference = big_val[~big_val.drawing.isin(small_val.drawing)]
+    for word, df in tqdm(difference.groupby("word")):
+        train_level2.append(df.iloc[:TRAIN_IMGS_PER_CLASS])
+
+    train_df = pd.concat(train_level2)
+    train_df.to_csv("level2_train.csv", index=False)
+
+    val_df = big_val[~big_val.drawing.isin(train_df.drawing)]
+    val_df.to_csv("level2_val.csv", index=False)
+
+    train_indices = big_val.index[big_val.drawing.isin(train_df.drawing)]
+    val_indices = big_val.index[big_val.drawing.isin(val_df.drawing)]
+    print(train_indices)
+    print(val_indices)
+
+    np.save("level2_train_indices", train_indices)
+    np.save("level2_val_indices", val_indices)
